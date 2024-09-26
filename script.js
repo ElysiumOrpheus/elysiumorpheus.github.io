0802e1e6f08a57405863a11887c37801f96de3e1
OwenOfarrell <143770801+OwenOfarrell@users.noreply.github.com>
2024-09-26 07:32:57 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ const marqueeMsgs =
 [
   "Our pasta, who art in a colander, draining be your noodles. Thy noodle come, Thy sauce be yum, on top some grated Parmesan. Give us this day, our garlic bread, …and forgive us our trespasses, as we forgive those who trample on our lawns.",
   "Icarus laughed as he fell. Threw his head back and yelled into the winds, arms spread wide, teeth bared to the world",
-  "It's giving bozo..."
+  "It's giving bozo...",
+  "What the sigma",
+  "Born to goon, forced to edge"
+  "Also try Terraria!"
 ]
 
 const marquee = document.getElementById("change");
